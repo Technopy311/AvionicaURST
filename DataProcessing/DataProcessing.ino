7cbbe5c00d3532cb9ff780973771c276c270ec5d
Technopy311 <xmenmagnet@gmail.com>
2024-05-17 21:29:03 -0400

    feat!(DataProcessing): Add feature to read and write Gyroscope/Accelerometer to SD Card

--- DataProcessing/DataProcessing.ino
+++ DataProcessing/DataProcessing.ino
@@ -1,8 +1,13 @@
-#include <Wire.h>
-#include "FastIMU.h"
 #include <Arduino.h>
+#include "FastIMU.h"
+#include <Wire.h>
+
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
 #define IMU_ADDRESS 0x68    //Change to the address of the IMU
-#define PERFORM_CALIBRATION //Comment to disable startup calibration
+//#define PERFORM_CALIBRATION //Comment to disable startup calibration
 MPU6500 IMU;               //Change to the name of any supported IMU! 
 
 // Currently supported IMUS: MPU9255 MPU9250 MPU6886 MPU6500 MPU6050 ICM20689 ICM20690 BMI055 BMX055 BMI160 LSM6DS3 LSM6DSL QMI8658
@@ -12,72 +17,105 @@ AccelData accelData;    //Sensor data
 GyroData gyroData;
 MagData magData;
 
+
+// SD card functions - from SD_Test esp32 SD library example.
+
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    //Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("OK");
+    } else {
+        Serial.println("None");
+    }
+    file.close();
+}
+
+char accelX[8];
+char accelY[8];
+char accelZ[8];
+
+char gyroX[8];
+char gyroY[8];
+char gyroZ[8];
+
+char timechar[8];
+
+unsigned long startTime = millis();
+
 void setup() {
+  Serial.begin(115200);
+
+  //## IMU Setup
   Wire.begin();
   Wire.setClock(400000); //400khz clock
-  Serial.begin(115200);
-  while (!Serial) {
-  }
-
   int err = IMU.init(calib, IMU_ADDRESS);
   if (err != 0) {
     Serial.print("Error initializing IMU: ");
     Serial.println(err);
     while (true) {
+      ;
     }
   }
   
-#ifdef PERFORM_CALIBRATION
-  Serial.println("FastIMU calibration & data example");
-  if (IMU.hasMagnetometer()) {
-    delay(1000);
-    Serial.println("Move IMU in figure 8 pattern until done.");
-    delay(3000);
-    IMU.calibrateMag(&calib);
-    Serial.println("Magnetic calibration done!");
-  }
-  else {
-    delay(5000);
-  }
+  #ifdef PERFORM_CALIBRATION
+    Serial.println("FastIMU calibration & data example");
+    if (IMU.hasMagnetometer()) {
+      delay(1000);
+      Serial.println("Move IMU in figure 8 pattern until done.");
+      delay(3000);
+      IMU.calibrateMag(&calib);
+      Serial.println("Magnetic calibration done!");
+    }
+    else {
+      delay(5000);
+    }
 
-  delay(5000);
-  Serial.println("Keep IMU level.");
-  delay(5000);
-  IMU.calibrateAccelGyro(&calib);
-  Serial.println("Calibration done!");
-  Serial.println("Accel biases X/Y/Z: ");
-  Serial.print(calib.accelBias[0]);
-  Serial.print(", ");
-  Serial.print(calib.accelBias[1]);
-  Serial.print(", ");
-  Serial.println(calib.accelBias[2]);
-  Serial.println("Gyro biases X/Y/Z: ");
-  Serial.print(calib.gyroBias[0]);
-  Serial.print(", ");
-  Serial.print(calib.gyroBias[1]);
-  Serial.print(", ");
-  Serial.println(calib.gyroBias[2]);
-  /*if (IMU.hasMagnetometer()) {
-    Serial.println("Mag biases X/Y/Z: ");
-    Serial.print(calib.magBias[0]);
+    delay(5000);
+    Serial.println("Keep IMU level.");
+    delay(5000);
+    IMU.calibrateAccelGyro(&calib);
+    Serial.println("Calibration done!");
+    Serial.println("Accel biases X/Y/Z: ");
+    Serial.print(calib.accelBias[0]);
     Serial.print(", ");
-    Serial.print(calib.magBias[1]);
+    Serial.print(calib.accelBias[1]);
     Serial.print(", ");
-    Serial.println(calib.magBias[2]);
-    Serial.println("Mag Scale X/Y/Z: ");
-    Serial.print(calib.magScale[0]);
+    Serial.println(calib.accelBias[2]);
+    Serial.println("Gyro biases X/Y/Z: ");
+    Serial.print(calib.gyroBias[0]);
     Serial.print(", ");
-    Serial.print(calib.magScale[1]);
+    Serial.print(calib.gyroBias[1]);
     Serial.print(", ");
-    Serial.println(calib.magScale[2]);
-  }*/
-  delay(5000);
-  IMU.init(calib, IMU_ADDRESS);
-#endif
+    Serial.println(calib.gyroBias[2]);
+    delay(5000);
+    IMU.init(calib, IMU_ADDRESS);
+  #endif
 
   //err = IMU.setGyroRange(500);      //USE THESE TO SET THE RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
-  //err = IMU.setAccelRange(2);       //THESE TWO SET THE GYRO RANGE TO ±500 DPS AND THE ACCELEROMETER RANGE TO ±2g
-  
+  //err = IMU.setAccelRange(2);       //THESE TWO SET THE GYRO RANGE TO ±500 DPS AND THE ACCELEROMETER RANGE TO ±2g  
   if (err != 0) {
     Serial.print("Error Setting range: ");
     Serial.println(err);
@@ -85,51 +123,66 @@ void setup() {
       ;
     }
   }
+
+
+  //## SD Card setup
+  if(!SD.begin()){
+        Serial.println("Card Mount Failed");
+        return;
+    }
+  uint8_t cardType = SD.cardType();
+
+  if(cardType == CARD_NONE){
+      Serial.println("No SD card attached");
+      return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+      Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+      Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+      Serial.println("SDHC");
+  } else {
+      Serial.println("UNKNOWN");
+  }
+
+  writeFile(SD, "/testing.csv", "Time,AccelX,AccelY,AccelZ,GyroX,GyroY,GyroZ");
+  appendFile(SD, "/testing.csv", "\n");
 }
 
 void loop() {
+  unsigned long currentTime = millis();
   IMU.update();
   IMU.getAccel(&accelData);
-  Serial.print("AccelX:");
-  Serial.print(accelData.accelX);
-  Serial.print(",");
-  Serial.print("AccelY:");
-  Serial.print(accelData.accelY);
-  Serial.print(",");
-  Serial.print("AccelZ:");
-  Serial.print(accelData.accelZ);
-  Serial.print(",");
-  
   IMU.getGyro(&gyroData);
-  Serial.print("GyroX:");
-  Serial.print(gyroData.gyroX);
-  Serial.print(",");
-  Serial.print("GyroY:");
-  Serial.print(gyroData.gyroY);
-  Serial.print(",");
-  Serial.print("GyroZ:");
-  Serial.println(gyroData.gyroZ);
-  /*if (IMU.hasMagnetometer()) {
-    IMU.getMag(&magData);
-    Serial.print(",");
-    Serial.print("Magnet X:");
-    Serial.print(magData.magX);
-    Serial.print(",");
-    Serial.print("Magnet Y:");
-    Serial.print(magData.magY);
-    Serial.print(",");
-    Serial.print("Magnet Z:");
-    Serial.print(magData.magZ);
-    Serial.print("\n");
-  }*/
-  /*
-  if (IMU.hasTemperature()) {
-	  Serial.print(",");
-	  Serial.println(IMU.getTemp());
-  }*/
 
-  
+  dtostrf(accelData.accelX, 6, 2, accelX);
+  dtostrf(accelData.accelY, 6, 2, accelY);
+  dtostrf(accelData.accelZ, 6, 2, accelZ);
+
+  dtostrf(gyroData.gyroX, 6, 2, gyroX);
+  dtostrf(gyroData.gyroY, 6, 2, gyroY);
+  dtostrf(gyroData.gyroZ, 6, 2, gyroZ);
 
 
-  delay(50);
+  unsigned long elapsedTime = currentTime-startTime;
+
+  dtostrf(elapsedTime, 6, 2, timechar);
+
+  appendFile(SD, "/testing.csv", timechar);
+  appendFile(SD, "/testing.csv", ",");
+  appendFile(SD, "/testing.csv", accelX);
+  appendFile(SD, "/testing.csv", ",");
+  appendFile(SD, "/testing.csv", accelY);
+  appendFile(SD, "/testing.csv", ",");
+  appendFile(SD, "/testing.csv", accelZ);
+  appendFile(SD, "/testing.csv", ",");
+  appendFile(SD, "/testing.csv", gyroX);
+  appendFile(SD, "/testing.csv", ",");
+  appendFile(SD, "/testing.csv", gyroY);
+  appendFile(SD, "/testing.csv", ",");
+  appendFile(SD, "/testing.csv", gyroZ);
+  appendFile(SD, "/testing.csv", "\n");
 }
